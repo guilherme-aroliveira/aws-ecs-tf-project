75d893bd15793593ea5c6778f26dd66efd0f0e77
Guilherme Oliveira <oliveira.g.ar@hotmail.com>
2025-02-18 12:16:19 -0300

    fix private route table configuration

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -120,7 +120,7 @@ resource "aws_route_table_association" "public" {
 }
 
 resource "aws_route_table_association" "private" {
-  for_each = aws_route_table.private_route_table
+  for_each = var.private_subnets
 
   subnet_id      = aws_subnet.private_subnets[each.key].id
   route_table_id = aws_route_table.private_route_table[each.key].id
