faf634cca0bd8d705e524db4c2d5b39b43a73bf8
Guilherme Oliveira <oliveira.g.ar@hotmail.com>
2025-02-18 10:03:00 -0300

    fix private route table configuration

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -97,7 +97,7 @@ resource "aws_route_table" "private_route_table" {
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway[each.value].id
+    nat_gateway_id = aws_nat_gateway.nat_gateway[each.key].id
   }
 
   tags = merge(
