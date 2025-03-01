745a2e9020684ad930b79ff9a36e26fe107173b1
Guilherme Oliveira <oliveira.g.ar@proton.me>
2025-03-01 15:51:08 -0300

    fix terraform configuration

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -98,7 +98,7 @@ resource "aws_route_table" "private_route_table" {
   route {
     cidr_block     = "0.0.0.0/0"
     #nat_gateway_id = aws_nat_gateway.nat_gateway.id
-    nat_gateway_id = aws_nat_gateway.nat_gateway[tolist(aws_subnet.public_subnets)[each.value % length(aws_subnet.public_subnets)]].id
+    nat_gateway_id = aws_nat_gateway.nat_gateway[each.key].id
   }
 
   tags = merge(
